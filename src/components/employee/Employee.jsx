a220b2c9cf92b4980cae002090b8f120c66f325d
Niyomufasha <fabrice1niyo@gmail.com>
2023-03-09 07:03:54 -0800

    display each employee

--- src/components/employee/Employee.jsx
+++ src/components/employee/Employee.jsx
@@ -1,22 +1,22 @@
-// import React from "react";
-// import { useEffect, useState } from "react";
-// import Employeedata from "./Employeedata";
+import React from "react";
+import { useEffect, useState } from "react";
+import Employeedata from "./Employeedata";
 
 
-// const Employee = () => {
-//   const [myEmployees, setMyEmployees] = useState([]);
+const Employee = () => {
+  const [myEmployees, setMyEmployees] = useState([]);
 
-//   useEffect(() => {
-//     fetch("http://localhost:3000/employees")
-//       .then((response) => response.json())
-//       .then((data) => setMyEmployees(data));
-//   }, []);
-//   return (
-//     <div>
-//       {myEmployees.map((value) => {
-//         return <Employeedata detail={value} key={value.id} />;
-//       })}
-//     </div>
-//   );
-// };
-// export default Employee;
+  useEffect(() => {
+    fetch("http://localhost:3000/employees")
+      .then((response) => response.json())
+      .then((data) => setMyEmployees(data));
+  }, []);
+  return (
+    <div>
+      {myEmployees.map((value) => {
+        return <Employeedata detail={value} key={value.id} />;
+      })}
+    </div>
+  );
+};
+export default Employee;
