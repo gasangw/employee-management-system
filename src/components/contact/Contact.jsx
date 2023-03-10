cc622c296d15af103dc73379e51ef4fe24519b09
Niyomufasha <fabrice1niyo@gmail.com>
2023-03-10 00:25:13 -0800

    create contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,22 @@
+import './contact.css'
 import React from 'react'
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import {MdMarkEmailUnread} from 'react-icons/md';
+import {BiWorld} from 'react-icons/bi';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Am in contact</h1>
+    <div class="contact">
+    
+      <div className='contactinfo'>
+      <div class="titlle"><b><h1>CONTACT<br/>INFOMATION</h1></b></div>
+      <hr />
+        <div class="detail">
+        <h3><span><BsFillTelephoneFill/></span>134-678-908</h3>
+        <h3><span><MdMarkEmailUnread/></span>employeemanagement12@gmail.com</h3>
+        <h3><span><BiWorld/></span>www.emanagementsystem.com</h3>
+        </div>
+      </div>
     </div>
   )
 }
